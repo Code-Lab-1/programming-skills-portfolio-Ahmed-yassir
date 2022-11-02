fa68dd728fc44f235a58e5067d59349f41c32403
Ahmed-yassir <ahm_coool@hotmail.com>
2022-11-02 13:32:22 +0400

    Update exercise1.py

---
+++ Chapter 1- Getting Started with Python/Exercises/exercise1.py
@@ -0,0 +1,1 @@
+print("Twinkle, twinkle,little star, \n\tHow i wonder what you are!\n\t\tup above the world so high, \n\t\tLike a diamond in the sky. \nTwinkle, Twinkle,little star,\n\tHow i wonder what you are")
