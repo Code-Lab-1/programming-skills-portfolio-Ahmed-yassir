88c26b256ece2792295e986ff189f4b3c2f229ec
Ahmed-yassir <ahm_coool@hotmail.com>
2022-11-07 10:32:01 +0400

    Create excercise4.py

---
+++ Chapter 3- Data Structures/Exercises/excercise4.py
@@ -0,0 +1,10 @@
+guests = ["yousif","musa","saeed","yassir","awad", "tayseer"]
+
+name = guests[0].title()
+print(name+ ", please come to lunch.")
+
+name = guests[1].title()
+print(name + ", please come to lunch")
+ 
+name = guests[2].title()
+print(name + ", please come to lunch.")
