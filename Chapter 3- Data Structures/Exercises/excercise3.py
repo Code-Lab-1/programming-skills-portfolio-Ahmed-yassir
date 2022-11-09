9ab65b56223bba7be62ab2c37bbc5b7cb5d14c74
Ahmed-yassir <ahm_coool@hotmail.com>
2022-11-09 13:39:34 +0400

    Create excercise3.py

---
+++ Chapter 3- Data Structures/Exercises/excercise3.py
@@ -0,0 +1,7 @@
+sports_motorcycle=["BMW motorrad", "Masito M322721", "New ray honda","Suzuki", "Yamaha"]
+print("I would like to get a",sports_motorcycle[0])
+print("I would like to get a",sports_motorcycle[1])
+print("I would like to get a",sports_motorcycle[2])
+print("I would like to get a",sports_motorcycle[3])
+print("I would like to get a",sports_motorcycle[4])
+print("I would like to get a",sports_motorcycle[5])
