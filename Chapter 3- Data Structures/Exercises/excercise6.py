cab123cfc7bd607aff07b7f1285134bed05cd80f
Ahmed-yassir <ahm_coool@hotmail.com>
2022-11-15 10:01:01 +0400

    Update excercise6.py

--- Chapter 3- Data Structures/Exercises/excercise6.py
+++ Chapter 3- Data Structures/Exercises/excercise6.py
@@ -1,11 +1,51 @@
-print(name +" , please come to lunch.")
+guests = ['Ali rashid', 'mark evan', 'faisal']
+
+name = guests[0].title()
+print(name + ", please come to dinner.")
 
 name = guests[1].title()
-print(name + " , please come to lunch.")
+print(name + ", please come to dinner.")
 
+name = guests[2].title()
+print(name + ", please come to dinner.")
 
-del(guests[0])
-del(guests[0])
+name = guests[1].title()
+print("\nSorry, " + name + " can't make it to dinner.")
 
+# Mark evan can't make it! Let's invite Rayan instead.
+del(guests[1])
+guests.insert(1, 'Rayan')
 
-print(guests)
+# Print the invitations again.
+name = guests[0].title()
+print("\n" + name + ", please come to dinner.")
+
+name = guests[1].title()
+print(name + ", please come to dinner.")
+
+name = guests[2].title()
+print(name + ", please come to dinner.")
+
+# We got a bigger table, so let's add some more people to the list.
+print("\nWe got a bigger table!")
+guests.insert(0, 'faris mohammed')
+guests.insert(2, 'noor saif')
+guests.append('Saif ahmed')
+
+name = guests[0].title()
+print(name + ", please come to dinner.")
+
+name = guests[1].title()
+print(name + ", please come to dinner.")
+
+name = guests[2].title()
+print(name + ", please come to dinner.")
+
+name = guests[3].title()
+print(name + ", please come to dinner.")
+
+name = guests[4].title()
+print(name + ", please come to dinner.")
+
+name = guests[5].title()
+print(name + ", please come to dinner.")
