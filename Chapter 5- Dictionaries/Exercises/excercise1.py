87be1baf958d5273919106c9697f960a4536b241
Ahmed-yassir <ahm_coool@hotmail.com>
2022-11-15 10:30:47 +0400

    Create excercise1.py

---
+++ Chapter 5- Dictionaries/Exercises/excercise1.py
@@ -0,0 +1,11 @@
+person = {
+    'first_name': 'Saif',
+    'last_name': 'Yousef',
+    'age': 30,
+    'city': 'Dubai',
+    }
+
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
